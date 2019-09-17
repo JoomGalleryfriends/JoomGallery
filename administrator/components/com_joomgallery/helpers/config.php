31b7c7f6239097e43fd140a2fe3123996f4ebe7b
MrMusic <webmaster@mrmusic63.de>
2019-09-17 11:12:16 +0200

    Add new config options in config.php

--- administrator/components/com_joomgallery/helpers/config.php
+++ administrator/components/com_joomgallery/helpers/config.php
@@ -50,6 +50,8 @@ class JoomConfig extends JObject
   var $jg_thumbwidth;
   var $jg_thumbheight;
   var $jg_thumbquality;
+  var $jg_upload_exif_rotation;
+  var $jg_originalquality;
   var $jg_uploadorder;
   var $jg_useorigfilename;
   var $jg_filenamenumber;
