9a57807c5abfa83863b1405c60766cc846ba6f87
MrMusic <webmaster@mrmusic63.de>
2019-11-07 15:51:45 +0100

    Fix 'Exif comment' value for ReplaceWithMetadata

--- administrator/components/com_joomgallery/includes/exifarray.php
+++ administrator/components/com_joomgallery/includes/exifarray.php
@@ -346,13 +346,6 @@ defined('_JEXEC') or die('Direct Access to this location is not allowed.');
                         'Format'      => "Unsigned Rational",
                         'Units'       => JText::_('COM_JOOMGALLERY_SUBIFD_FOCALLENGTH_UNIT')
                     ),
-      37510 => array(   'Attribute'   => "UserComment",
-                        'Name'        => JText::_('COM_JOOMGALLERY_SUBIFD_USERCOMMENT'),
-                        'Description' => JText::_('COM_JOOMGALLERY_SUBIFD_USERCOMMENT_DESCRIPTION'),
-                        'Calculation' => "",
-                        'Format'      => "Undefined",
-                        'Units'       => ""
-                    ),
 //       37520 => array(   'Attribute'   => "SubsecTime",
 //                         'Name'        => JText::_('COM_JOOMGALLERY_SUBIFD_SUBSECTIME'),
 //                         'Description' => JText::_('COM_JOOMGALLERY_SUBIFD_SUBSECTIME_DESCRIPTION'),
@@ -590,6 +583,17 @@ defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 //                         'Format'      => "",
 //                         'Units'       => ""
 //                     )
+      ),
+
+      "COMMENT" => array 
+      (
+      37510 => array(   'Attribute'   => "COMMENT",
+                        'Name'        => JText::_('COM_JOOMGALLERY_SUBIFD_USERCOMMENT'),
+                        'Description' => JText::_('COM_JOOMGALLERY_SUBIFD_USERCOMMENT_DESCRIPTION'),
+                        'Calculation' => "",
+                        'Format'      => "Undefined",
+                        'Units'       => ""
+                    ),
       ),
 
       "GPS" => array
