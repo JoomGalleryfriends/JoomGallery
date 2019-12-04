ecd76acfd95979b0884566f16d8f53f8a702583e
Erftralle <Erftralle@users.noreply.github.com>
2019-12-04 16:20:08 +0100

    Adjusting the copyright years

--- components/com_joomgallery/controllers/ajaxupload.raw.php
+++ components/com_joomgallery/controllers/ajaxupload.raw.php
@@ -2,7 +2,7 @@
 /****************************************************************************************\
 **   JoomGallery 3                                                                      **
 **   By: JoomGallery::ProjectTeam                                                       **
-**   Copyright (C) 2008 - 2013 JoomGallery::ProjectTeam                                **
+**   Copyright (C) 2008 - 2019  JoomGallery::ProjectTeam                                **
 **   Based on: JoomGallery 1.0.0 by JoomGallery::ProjectTeam                            **
 **   Released under GNU GPL Public License                                              **
 **   License: http://www.gnu.org/copyleft/gpl.html or have a look                       **
@@ -59,7 +59,7 @@ class JoomGalleryControllerAjaxupload extends JControllerLegacy
     {
       $result['debug_output'] = $debug_output;
     }
-    
+
     if($ifRedirect = $uploader->getIfRedirect())
     {
       $result['ifRedirect'] = $ifRedirect;
