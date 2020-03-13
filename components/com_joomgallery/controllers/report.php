902ff8582f0f4e9aa23c0813e95d6f496d2bfc19
MrMusic <webmaster@mrmusic63.de>
2020-03-13 10:51:58 +0100

    Fix warning in report view

--- components/com_joomgallery/controllers/report.php
+++ components/com_joomgallery/controllers/report.php
@@ -1,8 +1,8 @@
 <?php
 /****************************************************************************************\
-**   JoomGallery 3                                                                   **
+**   JoomGallery 3                                                                      **
 **   By: JoomGallery::ProjectTeam                                                       **
-**   Copyright (C) 2008 - 2019  JoomGallery::ProjectTeam                                **
+**   Copyright (C) 2008 - 2020  JoomGallery::ProjectTeam                                **
 **   Based on: JoomGallery 1.0.0 by JoomGallery::ProjectTeam                            **
 **   Released under GNU GPL Public License                                              **
 **   License: http://www.gnu.org/copyleft/gpl.html or have a look                       **
@@ -38,6 +38,7 @@ class JoomGalleryControllerReport extends JControllerLegacy
     $sstring  = JRequest::getVar('sstring');
 
     $redirect_url = 'index.php?view=detail&id='.$id;
+    $query_append = '';
     if($catid)
     {
       // Request was initiated by category view
