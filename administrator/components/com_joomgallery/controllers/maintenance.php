c0cb1c55b66e39c5290a1de362b76651fd14ae09
19leunam93 <tech.spuur@quickline.ch>
2021-10-25 09:56:11 +0200

    resolve conflicts

--- administrator/components/com_joomgallery/controllers/maintenance.php
+++ administrator/components/com_joomgallery/controllers/maintenance.php
@@ -32,7 +32,7 @@ class JoomGalleryControllerMaintenance extends JoomGalleryController
     // Access check
     if(!JFactory::getUser()->authorise('core.manage', _JOOM_OPTION))
     {
-      $this->setRedirect(JRoute::_($this->_ambit->getRedirectUrl(''), false), 'You are not allowed to manage this component', 'notice');
+      $this->setRedirect(JRoute::_($this->_ambit->getRedirectUrl(''), false), JText::_('COM_JOOMGALLERY_COMMON_MSG_NOT_ALLOWED_TO_CONFIGURE', 'notice'));
       $this->redirect();
     }
 
