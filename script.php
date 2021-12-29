5d19754e03d95ccffcbb312cfd0ddb27ab20eeb7
Elfangor93 <tech.spuur@quickline.ch>
2021-12-29 14:18:24 +0100

    change headers

--- script.php
+++ script.php
@@ -1,11 +1,12 @@
 <?php
-
 /**
- * @package    Com_Joomgallery
- * @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>
- * @copyright  2008 - 2022  JoomGallery::ProjectTeam
- * @license    GNU General Public License version 2 or later
- */
+******************************************************************************************
+**   @version    4.0.0                                                                  **
+**   @package    com_joomgallery                                                        **
+**   @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>                 **
+**   @copyright  2008 - 2022  JoomGallery::ProjectTeam                                  **
+**   @license    GNU General Public License version 2 or later                          **
+*****************************************************************************************/
 
 define('MODIFIED', 1);
 define('NOT_MODIFIED', 2);
--- site/src/View/Categoryform/HtmlView.php
+++ site/src/View/Categoryform/HtmlView.php
@@ -1,12 +1,13 @@
 <?php
 
 /**
- * @version    CVS: 4.0.0
- * @package    Com_Joomgallery
- * @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>
- * @copyright  2008 - 2022  JoomGallery::ProjectTeam
- * @license    GNU General Public License version 2 or later
- */
+******************************************************************************************
+**   @version    4.0.0                                                                  **
+**   @package    com_joomgallery                                                        **
+**   @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>                 **
+**   @copyright  2008 - 2022  JoomGallery::ProjectTeam                                  **
+**   @license    GNU General Public License version 2 or later                          **
+*****************************************************************************************/
 
 namespace Joomgallery\Component\Joomgallery\Site\View\Categoryform;
 // No direct access
