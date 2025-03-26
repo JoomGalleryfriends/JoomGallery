baf2683440179c4e50435130adfd36a3ef8a35a0
Elfangor <tech.spuur@quickline.ch>
2025-03-26 13:29:52 +0100

    add maxRowImgs to gallery view

--- site/com_joomgallery/src/Model/GalleryModel.php
+++ site/com_joomgallery/src/Model/GalleryModel.php
@@ -201,7 +201,7 @@ class GalleryModel extends JoomItemModel
     }
 
     // Load the number of images defined in the configuration
-    $listModel->setState('list.limit', $params['configs']->get('jg_gallery_view_numb_images', 12, 'int'));
+    $listModel->setState('list.limit', $params['configs']->get('jg_gallery_view_numb_images', 15, 'int'));
 
     // Apply number of images to be loaded from list in the view
     if(isset($imgform_list['limit']))
