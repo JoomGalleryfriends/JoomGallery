1bf99ca31d2a1b7435dec4a440ab6218b620e254
Elfangor <tech.spuur@quickline.ch>
2025-03-26 08:12:32 +0100

    fix error

--- media/com_joomgallery/js/joomgrid.js
+++ media/com_joomgallery/js/joomgrid.js
@@ -80,7 +80,7 @@ var callback = function() {
     const imgs = document.querySelectorAll('.' + window.joomGrid.gridclass + ' img');
     const options = {
       idealHeight: window.joomGrid.justified.height,
-      maxRowImgs: Math.ceil(num_images * 1.2),
+      maxRowImgs: Math.ceil(window.joomGrid.num_images * 1.2),
       rowGap: window.joomGrid.justified.gap,
       columnGap: window.joomGrid.justified.gap,
     };
