6e9a2a64a5420dc60c0aa6b12b1ceff52770dc5d
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2025-06-24 15:46:17 +0200

    cleaned up cli commands

--- administrator/com_joomgallery/src/CliCommand/CategoryAdd.php
+++ administrator/com_joomgallery/src/CliCommand/CategoryAdd.php
@@ -1,11 +1,13 @@
 <?php
 /**
-******************************************************************************************
-**   @package    com_joomgallery                                                        **
-**   @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>                 **
-**   @copyright  2008 - 2025  JoomGallery::ProjectTeam                                  **
-**   @license    GNU General Public License version 3 or later                          **
-*****************************************************************************************/
+ ******************************************************************************************
+ **   @package    com_joomgallery                                                        **
+ **   @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>                 **
+ **   @copyright  2008 - 2025  JoomGallery::ProjectTeam                                  **
+ **   @license    GNU General Public License version 3 or later            ToDo: Format  **
+ *****************************************************************************************/
+
+// WIP: in development, but can not be called from caommand line (plugin)
 
 namespace Joomgallery\Component\Joomgallery\Administrator\CliCommand;
 
--- administrator/com_joomgallery/src/CliCommand/CategoryList.php
+++ administrator/com_joomgallery/src/CliCommand/CategoryList.php
@@ -1,11 +1,11 @@
 <?php
 /**
-******************************************************************************************
-**   @package    com_joomgallery                                                        **
-**   @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>                 **
-**   @copyright  2008 - 2025  JoomGallery::ProjectTeam                                  **
-**   @license    GNU General Public License version 3 or later                          **
-*****************************************************************************************/
+ ******************************************************************************************
+ **   @package    com_joomgallery                                                        **
+ **   @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>                 **
+ **   @copyright  2008 - 2025  JoomGallery::ProjectTeam                                  **
+ **   @license    GNU General Public License version 3 or later                          **
+ *****************************************************************************************/
 
 namespace Joomgallery\Component\Joomgallery\Administrator\CliCommand;
 
@@ -21,8 +21,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-
-//use Joomla\CMS\MVC\Factory\MVCFactoryAwareTrait;
 
 class CategoryList extends AbstractCommand
 {
