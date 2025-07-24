9bd60a6a9e64d62b6b0bc8ba3d802006c95f9a5b
Elfangor <tech.spuur@quickline.ch>
2025-07-24 08:54:31 +0200

    add resume remembering

--- plugins/task/joomgallery/src/Extension/Joomgallery.php
+++ plugins/task/joomgallery/src/Extension/Joomgallery.php
@@ -9,7 +9,6 @@
 *****************************************************************************************/
 namespace Joomgallery\Plugin\Task\Joomgallery\Extension;
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\Event\SubscriberInterface;
 use Joomla\Component\Scheduler\Administrator\Task\Task;
@@ -67,7 +66,7 @@ final class Joomgallery extends CMSPlugin implements SubscriberInterface
    *
    * @return  integer  The routine exit code.
    *
-   * @since  4.1.0
+   * @since  4.2.0
    * @throws \Exception
    */
   private function recreate(ExecuteTaskEvent $event): int
@@ -77,7 +76,7 @@ final class Joomgallery extends CMSPlugin implements SubscriberInterface
     $params       = $event->getArgument('params');
     $lastStatus   = $task->get('last_exit_code', Status::OK);
     $max_time     = (int) \ini_get('max_execution_time');
-    $willResume   = false;
+    $willResume   = true;
     $webcron      = false;
     $app          = Factrory::getApplication();
 
@@ -106,6 +105,8 @@ final class Joomgallery extends CMSPlugin implements SubscriberInterface
     if(\count($ids) == 1 && $ids[0] == 0)
     {
       $this->logTask('Attempt to recreate all available images...');
+
+      //ToDo: get list of all possible image ids
     }
 
     // Load the model to perform the task
@@ -128,8 +129,15 @@ final class Joomgallery extends CMSPlugin implements SubscriberInterface
     }
 
     $assumed_duration = 1;
+    $executed_ids = \array_map('trim', \explode(',', $params->successful));
     foreach($ids as $id)
     {
+      // Skip the already executed ids
+      if(in_array($id, $executed_ids))
+      {
+        continue;
+      }
+
       // Check if we can still continue executing the task
       $execute_task = true;
       if($max_time !== 0)
@@ -153,6 +161,9 @@ final class Joomgallery extends CMSPlugin implements SubscriberInterface
           // We log failed recreations.
           $this->logTask(\sprintf('Recreation of images failed. Failed image: %s', $id));
         }
+
+        // Add id to executed ids array
+        array_push($executed_ids, $id);
       }
       else
       {
@@ -161,11 +172,22 @@ final class Joomgallery extends CMSPlugin implements SubscriberInterface
       }
     }
 
+    // Check if we are finished
+    if(\count($ids) == \count($executed_ids))
+    {
+      // We finished the job
+      $willResume = false;
+      $params->successful = [];
+    }
+
     // Log our intention to resume or not and return the appropriate exit code.
     if($willResume && !$webcron)
     {
+      // Write params with successful executed ids to database
+      $params->successful = \implode(',', $executed_ids);
+      $this->setParams($task->get('id'), $params);
+
       $this->logTask(\sprintf('Recreation of images (Task %d) will resume', $task->get('id')));
-      // ToDo: Store id where to resume
     }
     else
     {
@@ -174,5 +196,38 @@ final class Joomgallery extends CMSPlugin implements SubscriberInterface
     }
 
     return $willResume ? Status::WILL_RESUME : Status::OK;
+  }
+
+  /**
+   * Writes the params to the database
+   * 
+   * @param   int     $task_id  The id of the task
+   * @param   object  $params   The params object
+   * 
+   * @return  void
+   * 
+   * @since   4.2.0
+   */
+  private function setParams($task_id, $params)
+  {
+    $params = new Registry($params);
+
+    $query = $this->db->getQuery(true);
+
+    $query->update($this->db->quoteName('#__scheduler_tasks'))
+          ->set($this->db->quoteName('params') . ' = ' . $this->db->quote($params->toString('json')))
+          ->where($this->db->quoteName('id') . ' = :extension_id')
+          ->bind(':extension_id', $task_id, ParameterType::INTEGER);
+      
+    $this->db->setQuery($query);
+
+    try
+    {
+      $this->db->execute();
+    }
+    catch(\Exception $e)
+    {
+      $this->logTask(\sprintf('[Task ID %d] Error storing task params: ' . $e->getMessage(), $task_id));
+    }
   }
 }
