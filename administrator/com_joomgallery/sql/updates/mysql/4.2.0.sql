819af5acf4eee8f46534879603df5337b580a489
MrMusic <webmaster@mrmusic63.de>
2025-07-25 14:36:47 +0200

    Change max filesize to MB

--- administrator/com_joomgallery/sql/updates/mysql/4.2.0.sql
+++ administrator/com_joomgallery/sql/updates/mysql/4.2.0.sql
@@ -7,3 +7,5 @@ ALTER TABLE `#__joomgallery_tags_ref` ADD INDEX `idx_tag_img` (`tagid`, `imgid`)
 ALTER TABLE `#__joomgallery_collections_ref` ADD INDEX `idx_imgid` (`imgid`);
 ALTER TABLE `#__joomgallery_collections_ref` ADD INDEX `idx_collectionid` (`collectionid`);
 ALTER TABLE `#__joomgallery_collections_ref` ADD INDEX `idx_col_img` (`collectionid`, `imgid`);
+ALTER TABLE `#__joomgallery_configs` MODIFY COLUMN `jg_maxfilesize` DOUBLE NOT NULL DEFAULT 2;
+UPDATE `#__joomgallery_configs` SET `jg_maxfilesize` = `jg_maxfilesize` / 1000000;
