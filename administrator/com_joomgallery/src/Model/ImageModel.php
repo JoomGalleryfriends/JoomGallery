fc2c736315d14ca05bcf9b57fe0b9f0160bce4ce
19leunam93 <tech.spuur@quickline.ch>
2025-07-27 18:48:17 +0200

    fix issue in ImageModel::recreate()

--- administrator/com_joomgallery/src/Model/ImageModel.php
+++ administrator/com_joomgallery/src/Model/ImageModel.php
@@ -1150,7 +1150,6 @@ class ImageModel extends JoomAdminModel
       else
       {
         // Prune items that you can't change.
-        unset($pks[$i]);
         $error = $this->getError();
 
         if($error)
