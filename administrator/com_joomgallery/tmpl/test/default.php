472741b3e4bc5e137606660d4d52e14274b38201
19leunam93 <tech.spuur@quickline.ch>
2025-08-01 18:16:53 +0200

    add AND logic

---
+++ administrator/com_joomgallery/tmpl/test/default.php
@@ -0,0 +1,20 @@
+<?php
+/**
+******************************************************************************************
+**   @package    com_joomgallery                                                        **
+**   @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>                 **
+**   @copyright  2008 - 2025  JoomGallery::ProjectTeam                                  **
+**   @license    GNU General Public License version 3 or later                          **
+*****************************************************************************************/
+
+// No direct access
+defined('_JEXEC') or die;
+
+// Import CSS & JS
+$wa = $this->document->getWebAssetManager();
+$wa->useStyle('com_joomgallery.admin');
+?>
+
+<div class="jg-test">
+  <p>This view is for testing purposes only...</p>
+</div>
