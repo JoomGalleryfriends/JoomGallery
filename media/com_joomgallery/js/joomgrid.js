4819b1e65b3445c030d01ca4c6b8077c55b0fd27
Peter Reill <43771555+reilldesign@users.noreply.github.com>
2025-10-10 12:12:03 +0200

    [Feature] Enable hash function in LightGallery

--- media/com_joomgallery/js/joomgrid.js
+++ media/com_joomgallery/js/joomgrid.js
@@ -57,7 +57,7 @@ var callback = function() {
         zoom: settings.zoom,
         toggleThumb: true,
         speed: 500,
-        plugins: [lgThumbnail,lgZoom],
+        plugins: [lgHash,lgThumbnail,lgZoom],
         preload: 1,
         loop: false,
         slideEndAnimation: false,
