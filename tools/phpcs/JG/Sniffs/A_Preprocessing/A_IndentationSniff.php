71391aecce28ad382d8f5d20982287b81506f460
19leunam93 <tech.spuur@quickline.ch>
2025-12-01 18:46:52 +0100

    improve indentation sniff

--- tools/phpcs/JG/Sniffs/A_Preprocessing/A_IndentationSniff.php
+++ tools/phpcs/JG/Sniffs/A_Preprocessing/A_IndentationSniff.php
@@ -1,18 +1,32 @@
 <?php
+/**
+ * *********************************************************************************
+ *    @package    com_joomgallery                                                 **
+ *    @author     JoomGallery::ProjectTeam <team@joomgalleryfriends.net>          **
+ *    @copyright  2008 - 2025  JoomGallery::ProjectTeam                           **
+ *    @license    GNU General Public License version 3 or later                   **
+ * *********************************************************************************
+ */
+
 declare(strict_types=1);
 
 namespace PHP_CodeSniffer\Standards\JG\Sniffs\A_Preprocessing;
 
+use ColinODell\Indentation\Indentation;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
-use ColinODell\Indentation\Indentation;
 
-class A_IndentationSniff implements Sniff
+final class A_IndentationSniff implements Sniff
 {
   /**
    * Indent size you want to enforce
    */
   public int $indentSize = 2;
+
+  /**
+   * Make sure it runs only once per file
+   */
+  public static array $processedFiles = [];
 
   public function register()
   {
@@ -22,32 +36,51 @@ class A_IndentationSniff implements Sniff
 
   public function process(File $phpcsFile, $stackPtr)
   {
+    $filename = $phpcsFile->getFilename();
+
+    // Prevent running multiple times per file
+    if(isset(self::$processedFiles[$filename]))
+    {
+      return ($phpcsFile->numTokens + 1);
+    }
+    self::$processedFiles[$filename] = true;
+
     // Load whole content
-    //$content = \file_get_contents($phpcsFile->getFilename());
-    $content = $phpcsFile->getTokensAsString(0, $phpcsFile->numTokens - 1);
+    $content     = \file_get_contents($filename);
+    $content_det = $this->stripHeader($content);
+
+    // Fallback if no class/interface/trait/enum found
+    if($content_det === '')
+    {
+      $content_det = $content;
+    }
 
     // Detect tabs
-    $hasTabs = \strpos($content, "\t") !== false;
+    $hasTabs = \strpos($content_det, "\t") !== false;
 
     // Detect indentation
-    $indent = Indentation::detect($content);
+    $indent      = Indentation::detect($content_det);
     $currentSize = $indent->getAmount();
     $currentType = $indent->getType();
 
-    // If already OK, nothing to fix
-    if(!$hasTabs && $currentType === Indentation::TYPE_SPACE && $currentSize === $this->indentSize)
+    if($hasTabs || $currentType !== Indentation::TYPE_SPACE || ($currentSize > 1 && $currentSize !== $this->indentSize))
     {
+      // Lets fix it
+    }
+    else
+    {
+      // Already OK, nothing to fix
       return;
     }
 
     // Add an error with autofix enabled
     $fix = $phpcsFile->addFixableError(
-      sprintf(
-        'Indentation probably not correct. Expects %d spaces and no tabs.',
-        $this->indentSize
-      ),
-      $stackPtr,
-      'WrongIndentation'
+        sprintf(
+            'Indentation probably not correct. Expects %d spaces and no tabs.',
+            $this->indentSize
+        ),
+        $stackPtr,
+        'WrongIndentation'
     );
 
     if($fix === true)
@@ -56,24 +89,28 @@ class A_IndentationSniff implements Sniff
       $content = \str_replace("\t", "  ", $content);
 
       //Normalize mixed indents
-      $content = \preg_replace_callback('/^\s+/m', function($m) {
-        return \str_replace("\t", "  ", $m[0]);
-      }, $content);
+      $content = \preg_replace_callback(
+        '/^\s+/m',
+        function ($m) {
+          return \str_replace("\t", "  ", $m[0]);
+        },
+        $content
+      );
 
       // Fix indentation
-      $newIndent = new Indentation($this->indentSize, Indentation::TYPE_SPACE);
+      $newIndent  = new Indentation($this->indentSize, Indentation::TYPE_SPACE);
       $newContent = Indentation::change($content, $newIndent);
 
       // Replace entire file content
-      //\file_put_contents($phpcsFile->getFilename(), $newContent);
       $phpcsFile->fixer->beginChangeset();
 
       // Put the entire new content into the first token
       $phpcsFile->fixer->replaceToken(0, $newContent);
 
       // Clear all remaining tokens so old code doesn’t hang around
-      for ($i = 1; $i < $phpcsFile->numTokens; $i++) {
-          $phpcsFile->fixer->replaceToken($i, '');
+      for($i = 1; $i < $phpcsFile->numTokens; $i++)
+      {
+        $phpcsFile->fixer->replaceToken($i, '');
       }
 
       $phpcsFile->fixer->endChangeset();
@@ -81,5 +118,47 @@ class A_IndentationSniff implements Sniff
 
     // Only run sniff once per file
     return ($phpcsFile->numTokens + 1);
+  }
+
+  /**
+   * Removes all lines before the class/interface/trait/enum definition.
+   *
+   * @param   string   $content  The full contents of a PHP file.
+   * @return  string   The cleaned file content.
+   */
+  protected function stripHeader(string $content): string
+  {
+    $lines = \preg_split('/\R/', $content);
+
+    for($i = 0; $i < count($lines); $i++)
+    {
+      $line = $lines[$i];
+
+      // Skip lines above class definition
+      if(preg_match('/^\s*#\[.*\]/', $line))
+      {
+        continue;
+      }
+      if(preg_match('/^\s*\/\*\*/', $line))
+      {
+        continue;
+      }
+      if(preg_match('/^\s*\*/', $line))
+      {
+        continue;
+      }
+      if(preg_match('/^\s*\*\/\s*$/', $line))
+      {
+        continue;
+      }
+
+      // Class definition found
+      if(\preg_match('/^\s*(final\s+|abstract\s+)?(class|interface|trait|enum)\s+\w+/i', $line))
+      {
+        return \implode("\n", \array_slice($lines, $i));
+      }
+    }
+
+    return '';
   }
 }
