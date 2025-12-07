8e3a45d77320e5e2db20076792d0fcde395a6995
19leunam93 <tech.spuur@quickline.ch>
2025-12-07 09:38:08 +0100

    show extensions list by default

--- administrator/com_joomgallery/tmpl/control/default.php
+++ administrator/com_joomgallery/tmpl/control/default.php
@@ -648,12 +648,12 @@ function DisplayOfficialExtensions($manifest)
     <div class="accordion" id="<?php echo $id; ?>">
       <div class="accordion-item">
         <h2 class="accordion-header" id="<?php echo $itemId; ?>Header">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+          <button class="accordion-button" type="button" data-bs-toggle="collapse"
             data-bs-target="#<?php echo $itemId; ?>" aria-expanded="false" aria-controls="<?php echo $itemId; ?>">
             <?php echo Text::_('COM_JOOMGALLERY_CONTROL_EXTENSIONS'); ?>
           </button>
         </h2>
-        <div id="<?php echo $itemId; ?>" class="accordion-collapse collapse"
+        <div id="<?php echo $itemId; ?>" class="accordion-collapse collapse show"
           aria-labelledby="<?php echo $itemId; ?>Header" data-bs-parent="#<?php echo $id; ?>">
           <div class="accordion-body">
             <table class="table table-striped">
