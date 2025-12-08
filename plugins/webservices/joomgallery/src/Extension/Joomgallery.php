3fcc3fa1b798f59bd8bc57b61f4f56c45a4ae6af
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2025-12-08 12:08:04 +0100

    fix code style by batch

--- plugins/webservices/joomgallery/src/Extension/Joomgallery.php
+++ plugins/webservices/joomgallery/src/Extension/Joomgallery.php
@@ -27,84 +27,84 @@ use Joomla\Router\Route;
  */
 final class Joomgallery extends CMSPlugin implements SubscriberInterface
 {
-    /**
-     * Returns an array of events this subscriber will listen to.
-     *
-     * @return  array
-     *
-     * @since   5.1.0
-     */
-    public static function getSubscribedEvents(): array
-    {
-        return [
-          'onBeforeApiRoute' => 'onBeforeApiRoute',
-        ];
-    }
+  /**
+   * Returns an array of events this subscriber will listen to.
+   *
+   * @return  array
+   *
+   * @since   5.1.0
+   */
+  public static function getSubscribedEvents(): array
+  {
+    return [
+      'onBeforeApiRoute' => 'onBeforeApiRoute',
+    ];
+  }
 
-    /**
-     * Registers com_joomgallery's API's routes in the application
-     *
-     * @param   BeforeApiRouteEvent  $event  The event object
-     *
-     * @return  void
-     *
-     * @since   4.0.0
-     */
-    public function onBeforeApiRoute(BeforeApiRouteEvent $event): void
-    {
-        $router = $event->getRouter();
+  /**
+   * Registers com_joomgallery's API's routes in the application
+   *
+   * @param   BeforeApiRouteEvent  $event  The event object
+   *
+   * @return  void
+   *
+   * @since   4.0.0
+   */
+  public function onBeforeApiRoute(BeforeApiRouteEvent $event): void
+  {
+    $router = $event->getRouter();
 
-        $defaults = ['component' => 'com_joomgallery'];
-        // $getDefaults = array_merge(['public' => false], $defaults);
-        $getDefaults = array_merge(['public' => false], $defaults); // ToDo: Remove when tests finished, enables access without token
+    $defaults = ['component' => 'com_joomgallery'];
+    // $getDefaults = array_merge(['public' => false], $defaults);
+    $getDefaults = array_merge(['public' => false], $defaults); // ToDo: Remove when tests finished, enables access without token
 
 //          new Route(['GET'], 'v1/example/items/:slug', 'item.displayItem',
 //              ['slug' => '(.*)'], ['option' => 'com_example']),
 
+$router->addRoutes(
+    [
+      new Route(['GET'], 'v1/joomgallery/version', 'version.display', [], $getDefaults),
+    ]
+);
+
+    // joomla part of JG (not much there)
     $router->addRoutes(
         [
-          new Route(['GET'], 'v1/joomgallery/version', 'version.display', [], $getDefaults),
+          new Route(['GET'], 'v1/joomgallery/config_in_j', 'configinj.display', [], $getDefaults),
         ]
     );
-
-        // joomla part of JG (not much there)
-        $router->addRoutes(
-            [
-              new Route(['GET'], 'v1/joomgallery/config_in_j', 'configinj.display', [], $getDefaults),
-            ]
-        );
 
 //      $router->addRoutes([
 //          new Route(['GET'], 'v1/joomgallery', 'joomgallery.displayItem', [], $getDefaults),
 //      ]);
 
-        $router->createCRUDRoutes(
-            'v1/joomgallery/categories',
-            'categories',
-            ['component' => 'com_joomgallery'],
-            true // ToDo: Remove when tests finished
-        );
+    $router->createCRUDRoutes(
+        'v1/joomgallery/categories',
+        'categories',
+        ['component' => 'com_joomgallery'],
+        true // ToDo: Remove when tests finished
+    );
 
-        $router->createCRUDRoutes(
-            'v1/joomgallery/images',
-            'images',
-            ['component' => 'com_joomgallery'],
-            true // ToDo: Remove when tests finished
-        );
+    $router->createCRUDRoutes(
+        'v1/joomgallery/images',
+        'images',
+        ['component' => 'com_joomgallery'],
+        true // ToDo: Remove when tests finished
+    );
 
-        $router->createCRUDRoutes(
-            'v1/joomgallery/configs',
-            'configs',
-            ['component' => 'com_joomgallery'],
-            true // ToDo: Remove when tests finished
-        );
+    $router->createCRUDRoutes(
+        'v1/joomgallery/configs',
+        'configs',
+        ['component' => 'com_joomgallery'],
+        true // ToDo: Remove when tests finished
+    );
 
 
-         // custom fields
-        // $this->createFieldsRoutes($router);
+     // custom fields
+    // $this->createFieldsRoutes($router);
 
   //      $this->createContentHistoryRoutes($router);
-    }
+  }
 
 //    /**
 //     * Create fields routes
@@ -142,30 +142,30 @@ final class Joomgallery extends CMSPlugin implements SubscriberInterface
 //        );
 //    }
 
-    /**
-     * Create contenthistory routes
-     *
-     * @param   ApiRouter  &$router  The API Routing object
-     *
-     * @return  void
-     *
-     * @since   4.0.0
-     */
-    private function createContentHistoryRoutes(&$router): void
-    {
-        $defaults    = [
-          'component'  => 'com_contenthistory',
-          'type_alias' => 'com_joomgallery.joomgallery',
-          'type_id'    => 1,
-        ];
-        $getDefaults = array_merge(['public' => false], $defaults);
+  /**
+   * Create contenthistory routes
+   *
+   * @param   ApiRouter  &$router  The API Routing object
+   *
+   * @return  void
+   *
+   * @since   4.0.0
+   */
+  private function createContentHistoryRoutes(&$router): void
+  {
+    $defaults    = [
+      'component'  => 'com_contenthistory',
+      'type_alias' => 'com_joomgallery.joomgallery',
+      'type_id'    => 1,
+    ];
+    $getDefaults = array_merge(['public' => false], $defaults);
 
-        $routes = [
-          new Route(['GET'], 'v1/joomgallery/:id/contenthistory', 'history.displayList', ['id' => '(\d+)'], $getDefaults),
-          new Route(['PATCH'], 'v1/joomgallery/:id/contenthistory/keep', 'history.keep', ['id' => '(\d+)'], $defaults),
-          new Route(['DELETE'], 'v1/joomgallery/:id/contenthistory', 'history.delete', ['id' => '(\d+)'], $defaults),
-        ];
+    $routes = [
+      new Route(['GET'], 'v1/joomgallery/:id/contenthistory', 'history.displayList', ['id' => '(\d+)'], $getDefaults),
+      new Route(['PATCH'], 'v1/joomgallery/:id/contenthistory/keep', 'history.keep', ['id' => '(\d+)'], $defaults),
+      new Route(['DELETE'], 'v1/joomgallery/:id/contenthistory', 'history.delete', ['id' => '(\d+)'], $defaults),
+    ];
 
-        $router->addRoutes($routes);
-    }
+    $router->addRoutes($routes);
+  }
 }
