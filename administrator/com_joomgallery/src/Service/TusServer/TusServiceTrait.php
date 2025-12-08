00f11c7a21b6a6bd0b22849a42f8ed157e8d0188
19leunam93 <tech.spuur@quickline.ch>
2025-12-08 11:12:19 +0100

    fix tusserver

--- administrator/com_joomgallery/src/Service/TusServer/TusServiceTrait.php
+++ administrator/com_joomgallery/src/Service/TusServer/TusServiceTrait.php
@@ -26,60 +26,60 @@ use Joomla\CMS\Uri\Uri;
  */
 trait TusServiceTrait
 {
-  /**
-   * Storage for the Server class.
-   *
-   * @var ServerInterface
-   *
-   * @since  4.0.0
-   */
-  private $tus = null;
+    /**
+     * Storage for the Server class.
+     *
+     * @var ServerInterface
+     *
+     * @since  4.0.0
+     */
+    private $tus = null;
 
-  /**
-   * Returns the tus server class.
-   *
-   * @return  ServerInterface
-   *
-   * @since  4.0.0
-   */
-  public function getTusServer(): ServerInterface
-  {
-    return $this->tus;
-  }
-
-  /**
-   * Creates the tus server class
-   *
-   * @param   string   Upload folder path
-   * @param   string   TUS server implementation location (URI)
-   * @param   bool     True if debug mode should be activated
-   *
-   * @return  void
-   *
-   * @since  4.0.0
-   */
-  public function createTusServer(string $folder = '', string $location = '', bool $debug = false): void
-  {
-    // Create and configure server
-    if(empty($folder))
+    /**
+     * Returns the tus server class.
+     *
+     * @return  ServerInterface
+     *
+     * @since  4.0.0
+     */
+    public function getTusServer(): ServerInterface
     {
-      $folder = Factory::getApplication()->get('tmp_path');
+        return $this->tus;
     }
 
-    if(empty($location))
+    /**
+     * Creates the tus server class
+     *
+     * @param   string   Upload folder path
+     * @param   string   TUS server implementation location (URI)
+     * @param   bool     True if debug mode should be activated
+     *
+     * @return  void
+     *
+     * @since  4.0.0
+     */
+    public function createTusServer(string $folder = '', string $location = '', bool $debug = false): void
     {
-      if(factory::getapplication()->isclient('site'))
-      {
-        $location = uri::root(true) . '/index.php?option=com_joomgallery&task=userupload.tusupload';
-      }
-      else
-      {
-        $location = uri::root(true) . '/administrator/index.php?option=com_joomgallery&task=images.tusupload';
-      }
+        // Create and configure server
+        if(empty($folder))
+        {
+            $folder = Factory::getApplication()->get('tmp_path');
+        }
+
+        if(empty($location))
+        {
+          if(factory::getapplication()->isclient('site'))
+          {
+            $location = uri::root(true) . '/index.php?option=com_joomgallery&task=userupload.tusupload';
+          }
+          else
+          {
+            $location = uri::root(true) . '/administrator/index.php?option=com_joomgallery&task=images.tusupload';
+          }
+        }
+
+        $this->tus = new Server($folder, $location, $debug);
+
+        return;
     }
-
-    $this->tus = new Server($folder, $location, $debug);
-
-    return;
-  }
 }
