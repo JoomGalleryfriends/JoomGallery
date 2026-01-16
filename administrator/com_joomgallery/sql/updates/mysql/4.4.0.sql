a5871790c2c896f7b79ad55b4f66deeb74ab2839
gabrielbuckland <gabriel@buckland.li>
2026-01-16 11:49:44 +0100

    Refactor: update tasks table to remove unused fields

--- administrator/com_joomgallery/sql/updates/mysql/4.4.0.sql
+++ administrator/com_joomgallery/sql/updates/mysql/4.4.0.sql
@@ -1,20 +1,10 @@
-CREATE TABLE IF NOT EXISTS `#__joomgallery_tasks` (
-`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`title` VARCHAR(255) NOT NULL DEFAULT "",
-`taskid` INT(11) UNSIGNED NOT NULL DEFAULT 0,
-`type` VARCHAR(128) NOT NULL DEFAULT "",
-`last_execution` DATETIME DEFAULT NULL COMMENT 'timestamp of last run',
-`times_executed` INT(11) UNSIGNED DEFAULT 0 COMMENT 'count of successful runs',
-`params` TEXT NOT NULL,
-`published` TINYINT(1) NOT NULL DEFAULT 1,
-`ordering` INT(11) NOT NULL DEFAULT 0,
-`note` TEXT NOT NULL,
-`created_time` DATETIME NOT NULL,
-`checked_out` INT(11) UNSIGNED NOT NULL DEFAULT 0,
-`checked_out_time` DATETIME DEFAULT NULL,
-PRIMARY KEY (`id`),
-KEY `idx_type` (`type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+ALTER TABLE `#__joomgallery_tasks`
+DROP COLUMN `queue`,
+DROP COLUMN `successful`,
+DROP COLUMN `failed`,
+DROP COLUMN `counter`,
+DROP COLUMN `last_id`,
+DROP COLUMN `completed`;
 
 CREATE TABLE IF NOT EXISTS `#__joomgallery_task_items` (
 `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
