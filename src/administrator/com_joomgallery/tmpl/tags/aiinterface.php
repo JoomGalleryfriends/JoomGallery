d2d7e4bea5621d3ea71ea0df79f147f5b2cf11f6
19leunam93 <tech.spuur@quickline.ch>
2026-03-30 21:27:38 +0200

    improve genKeywords()

--- src/administrator/com_joomgallery/tmpl/tags/aiinterface.php
+++ src/administrator/com_joomgallery/tmpl/tags/aiinterface.php
@@ -69,8 +69,8 @@ $form_url = 'index.php?option=com_joomgallery&view=tags&layout=aiinterface&tmpl=
 
 // Initialize AIinterface
 $opts = [ 'prefix' => 'jgai',
-          'host' => 'http://localhost/api/v1',
-          'token' => 'jfhrujr:jurur',
+          'host' => $config->get('jg_aiint_host', 'http://localhost/api/v1'),
+          'token' => $config->get('jg_aiint_key', ''),
           'client_name' => 'JG-General',
           'autoload' => true,
           'configs' => [
@@ -78,6 +78,11 @@ $opts = [ 'prefix' => 'jgai',
             'def_lang' => Factory::getLanguage()->getTag(),
             'session' => Session::getFormToken(),
             'base_url' => Uri::base(),
+            'imagetype' => $config->get('jg_aiint_tags_imagetype', 'detail'),
+            'resize' => $config->get('jg_aiint_tags_maxdim', 500),
+            'max_parallel' => $config->get('jg_parallelprocesses', 1),
+            'case_sensitivity' => $config->get('jg_aiint_tags_casesens', 1),
+            'letter_case' => $config->get('jg_aiint_tags_caseupper', 1),
           ]
         ];
 $this->document->addScriptOptions('com_joomgallery.aiinterface', $opts);
