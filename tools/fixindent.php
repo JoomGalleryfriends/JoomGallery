947a478691b7a76f4aaf0f49aa0b3674856db388
19leunam93 <tech.spuur@quickline.ch>
2026-08-17 20:55:03 +0200

    check all files and folder sin src/

--- tools/fixindent.php
+++ tools/fixindent.php
@@ -19,8 +19,8 @@ $indentSize = 2;
 $indentType = Indentation::TYPE_SPACE;
 $doFix = false;
 $details = false;
-$folders = ['src/administrator', 'src/site', 'src/plugins'];
-$exclude = ['.git', 'vendor', 'includes', 'node_modules', 'cache'];
+$folders = ['src'];
+$exclude = ['.git', 'vendor', 'includes', 'media', 'node_modules', 'cache'];
 //----------------------
 
 // If script is called with "fix" argument → enable fixing
