e3b7005c502e8faafc405fd3791db85d8a595dae
19leunam93 <tech.spuur@quickline.ch>
2026-08-23 12:40:07 +0200

    fix code style

--- src/administrator/com_joomgallery/src/Service/Metadata/MetadataPHP.php
+++ src/administrator/com_joomgallery/src/Service/Metadata/MetadataPHP.php
@@ -507,14 +507,14 @@ class MetadataPHP extends BaseMetadata implements MetadataInterface
     {
       $parts = array_map('trim', explode(',', $entry));
 
-      $rationals = array_map(
+    $rationals = array_map(
         static function (string $part): array {
           [$numerator, $denominator] = array_map('intval', explode('/', $part, 2));
 
             return [$numerator, $denominator];
         },
         $parts
-      );
+    );
 
       return \count($rationals) === 1 ? $rationals[0] : $rationals;
     }
